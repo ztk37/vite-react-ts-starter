0dc0a7d31e012037843b525ed393b5b76b68b2df
ztk37 <ztk37.contact@gmail.com>
2021-03-12 18:55:37 +0100

    init

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+
+const Root = () => null;
+
+render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
